0615b14a2b4e04e1ba5489fb234c3b7be35b35f3
Matt-Pepper <matthew.g.pepper@gmail.com>
2022-10-10 00:09:38 +1100

    open, close windows from icon working

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
-const maximizeUI = (e) => {
+const maximizeWindow = (e) => {
 	//e.target.classList.toggle('maximized');
 	e.target.closest(".window-ui").classList.toggle("maximized");
+	e.target.closest(".window-ui").removeAttribute("style");
+    
+    e.target.parentNode.querySelector("i").classList.toggle("fa-window-maximize");
+    e.target.parentNode.querySelector("i").classList.toggle("fa-window-restore");
 };
 
-const button = document.querySelectorAll(".titlebar__maximizeButton");
-console.log(button);
-
-for (let index = 0; index < button.length; index++) {
-	button[index].addEventListener("click", maximizeUI, false);
+const closeWindow = (e) => {
+    e.target.closest(".window-ui").remove();
 }
 
-//const windowUi = document.querySelectorAll('.window-ui');
+
+const createTitleBarEvents = () => {
+    const maximizeButton = document.querySelectorAll(".titlebar__maximizeButton");
+    const closeButton = document.querySelectorAll(".titlebar__closeButton");
+    for (let i = 0; i < maximizeButton.length; i++) {
+        maximizeButton[i].addEventListener("click", maximizeWindow, false);
+        closeButton[i].addEventListener("click", closeWindow, false);
+    }
+}
+
+const windowUi = document.querySelectorAll('.window-ui');
 
 const getCurrentTime = () => {
 	const currentTime = Date.now();
@@ -21,6 +32,7 @@ const getCurrentTime = () => {
 	time.push(time[0] <= 12 ? "AM" : "PM");
 	if (time[0] > 12) time[0] -= 12;
 	if (time[0] == 0) time[0] = 12;
+    if (time[1] < 10) time[1] = `0${time[1]}` 
 
 	const timeString = `${time[0]}:${time[1]} ${time[2]}`;
 
@@ -28,3 +40,31 @@ const getCurrentTime = () => {
 };
 getCurrentTime();
 setInterval(getCurrentTime, 6000);
+
+
+const icon = document.querySelectorAll(".desktop-icon");
+const desktop = document.querySelector("#desktop");
+const windowTemplate = document.getElementById("windowUi");
+
+const createIconClickHandler = (icon) => {
+    for (let i = 0; i < icon.length; i++) {
+        icon[i].addEventListener("click", changeIconColor);
+        icon[i].addEventListener("dblclick", createWindow);
+
+    }
+}
+
+const createWindow = (e) => {
+    const windowClone = windowTemplate.content.firstElementChild.cloneNode(true);
+
+    desktop.appendChild(windowClone);
+    
+    createTitleBarEvents();
+
+}
+
+const changeIconColor = (e) => {
+    e.target.parentNode.querySelector(".desktop-icon__text").classList.toggle("desktop-icon--clicked");
+    e.target.parentNode.querySelector(".desktop-icon__img").classList.toggle("desktop-icon__img--clicked");
+}
+createIconClickHandler(icon);
